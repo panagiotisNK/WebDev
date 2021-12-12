31adecb274cc3d9a5d88adee1d0e66096f31f24d
Panagiotis Nikoloudis <pnikol2000@gmail.com>
2021-12-12 22:01:19 +0200

    Update json_to_db.php

--- json_to_db.php
+++ json_to_db.php
@@ -8,23 +8,29 @@ $json_data= file_get_contents($filename);
 
 $array= json_decode($json_data, true);
 
-foreach ($array as $row) {
+foreach ($array as $row) 
+{
 
-    $sql_basics = "INSERT INTO poi(poiId,poiName,poiAddress,poiRating,poiRatingn,poiCurrPop) VALUES( '".$row["id"]."' , '".$row["name"]."' , '".$row["address"]."' , '".$row["rating"]."' , '".$row["rating_n"]."' , '".$row["current_popularity"]."' )";
-    $sql_coordinates= "INSERT INTO poicoordinates(poiId,lat,lng) VALUES( '".$row["id"]."' , '".$row["coordinates"]["lat"]."' , '".$row["coordinates"]["lng"]."' )";
+$type_array = array_column($row["types"],NULL,NULL);
     
-    $sql_types= "INSERT INTO poitypes(poiId,poiType) VALUES( '".$row["id"]."' , '".$row["types"][i]."' )"; 
+
+$sql_basics = "INSERT INTO poi(poiId,poiName,poiAddress,poiRating,poiRatingn,poiCurrPop) VALUES( '".$row["id"]."' , '".$row["name"]."' , '".$row["address"]."' , '".$row["rating"]."' , '".$row["rating_n"]."' , '".$row["current_popularity"]."' )";
+$sql_coordinates= "INSERT INTO poicoordinates(poiId,lat,lng) VALUES( '".$row["id"]."' , '".$row["coordinates"]["lat"]."' , '".$row["coordinates"]["lng"]."' )";
 
     
-    mysqli_query($connect,$sql_basics);
-    mysqli_query($connect,$sql_coordinates);
-    for($i =0; $i < $type.length; $i++) {
-    mysqli_query($connect,$sql_types);
-    }
+mysqli_query($connect,$sql_basics);
+mysqli_query($connect,$sql_coordinates);
+for($i =0; $i <=sizeof($type_array)-1; $i++) 
+{
+$sql_types= "INSERT INTO poitypes(poiId,poiType) VALUES( '".$row["id"]."' , '".$row["types"][$i]."' )";
+mysqli_query($connect,$sql_types);
 }
 
-
+}
 
 
 echo "Data was inserted.";
 ?>
+
+
+
