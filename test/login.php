8fa00a9da460c933f8a8f3d97b6aeed726f309b4
myrtwdel <66162056+myrtwdel@users.noreply.github.com>
2022-08-29 21:15:47 +0300

    Update login.php
    prospathw na to ftiaksw olh mera kai de douleuei me tipota

--- test/login.php
+++ test/login.php
@@ -35,22 +35,24 @@
                                     
                                     <br><br>
                                     
-                                    <p class="text-white-50 mb-5">Enter your Email and Password to Log In.</p>
+                                    <p class="text-white-50 mb-5">Enter your Username and Password to Log In.</p>
   
                                     <div class="form-outline form-white mb-4">
-                                        <label class="form-label" for="typeEmailX">Email</label>
-                                        <input type="email" id="typeEmailX" class="form-control form-control-lg" />
+                                        <label class="form-label">Username</label>
+                                        <input type="text" name="username" class="form-control form-control-lg" />
                                     </div>
                                     
                                     <div class="form-outline form-white mb-4">
-                                        <label class="form-label" for="typePasswordX">Password</label>
-                                        <input type="password" id="typePasswordX" class="form-control form-control-lg" />
+                                        <label class="form-label">Password</label>
+                                        <input type="password" name="password" class="form-control form-control-lg" />
                                         <i class="bi bi-eye-slash" id="togglePassword"></i>
                                     </div>
   
                                     <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="#!">Forgot password?</a></p>
-  
-                                    <button class="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
+
+                                    <div class="form-outline form-white mb-4">    
+                                        <button class="btn btn-outline-light btn-lg px-5" type="submit" name="login_btn">Login</button>
+                                    </div>
 
                                     <div class="d-flex justify-content-center text-center mt-4 pt-1">
                                         <a href="#!" class="text-white">
