1671c37cc411da59456c4b08a73cb0e4f25784c1
myrtwdel <66162056+myrtwdel@users.noreply.github.com>
2022-08-31 15:04:13 +0300

    css change

--- dokimh4/login.php
+++ dokimh4/login.php
@@ -5,16 +5,9 @@
 <head>
 	<title>Log In | Covid Heat Maps</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="style4.css">
+	<link rel="stylesheet" type="text/css" href="style5.css">
 </head>
 <body>
-
-<nav class="navbar navbar-expand-lg bg-dark navbar-dark">
-	<div class="container">
-		<a href="#" class="navbar-brand">Covid Heat Maps</a>
-	</div>
-</nav>
-
 	<div class="header">
 		<h2>Login</h2>
 	</div>
