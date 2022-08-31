63e13065ec93056932489c8c964741e416746cef
myrtwdel <66162056+myrtwdel@users.noreply.github.com>
2022-08-31 16:39:25 +0300

    responsive
    eixa ksexasei na to kanw responsive

--- dokimh4/login.php
+++ dokimh4/login.php
@@ -3,15 +3,22 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 	<title>Log In | Covid Heat Maps</title>
+
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	<link rel=”stylesheet” href=”https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css” />
-<!--	<link rel="stylesheet" type="text/css" href="style5.css"> -->
+
 </head>
+
+
 <body>
 	<section class="vh-100 gradient-custom bg-dark">
-		<form class="row d-flex justify-content-center align-items-center h-100" method="post" action="login.php">
-			<div class="col-12 col-md-8 col-lg-6 col-xl-5">
+	<form class="container" method="post" action="login.php">
+		<div class="row d-flex justify-content-center align-items-center h-100">	
+		<div class="col-12 col-md-8 col-lg-6 col-xl-5">
 				<div class="card bg-dark text-white" style="border-radius: 1rem;">
 					<div class="card-body p-5 text-center">
 						<div class="mb-md-5 mt-md-4 pb-5">
@@ -43,6 +50,7 @@
 					</div>
 				</div>
 			</div>
+</div>
 		</form>
 	</section>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
